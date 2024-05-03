f76bfad0c6daa76ca533e1f7d2e3818b9791dad3
vinodwadkar2106 <vinod.wadkar@gmail.cm>
2024-05-03 23:26:29 +0530

    comitted

---
+++ Canteen.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Canteen.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public LoginController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
